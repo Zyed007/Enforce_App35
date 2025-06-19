72d4bf5006a64ec99de1553fba6ce4039de33a80
Athar Shafi <atharshafi5714@gmail.com>
2025-06-19 09:23:10 +0400

    Runtime Error

--- ios/Enforce.swift
+++
@@ -1,9 +0,0 @@
-//
-//  Enforce.swift
-//  EnForce
-//
-//  Created by Aravind on 17/11/20.
-//  Copyright © 2020 Facebook. All rights reserved.
-//
-
-import Foundation
---
+++ test.js
@@ -0,0 +1,11 @@
+// App.js (Temporary test)
+import React from 'react';
+import { View, Text } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello EnForce!</Text>
+    </View>
+  );
+}
